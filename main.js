5d9aeaac867578deb61f6191e35001a828b41b23
Sarah Rosanna Busch <annarose0113@gmail.com>
2022-03-08 21:58:30 -0800

    Update main.js

--- main.js
+++ main.js
@@ -2,19 +2,22 @@
 
 /** @brief webapp using spa framework
  *  @author Sarah Rosanna Busch
+ *  @date 8 March 2022
  */
 
 var main = (function() {
     var that = {}; //public methods
-    var vars = {}; //variables 'public' to all functions in main
-    var elem = {}; // store dom elems once on load for efficiency
+    var vars = {}; //variables global to main
+    var elem = {}; //store dom elems once on load for efficiency
 
+    //called on page load
     that.init = function() {
         elem.header = f.html.getElem('header');
         elem.main = f.html.getElem('main');
         elem.footer = f.html.getElem('footer');
     }
 
+    //request and render only the portion of the page that's new
     that.nav = function() {
         f.http.get('pages/htmlTags.html', function(pageData) {
             elem.main.innerHTML = pageData;
